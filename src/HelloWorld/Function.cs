773ac1d211481dc275ed80d9e38809488c240216
MarkSelyukov <33294280+MarkSelyukov@users.noreply.github.com>
2020-01-28 15:41:37 -0600

    nuget update

--- src/HelloWorld/Function.cs
+++ src/HelloWorld/Function.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 using Amazon.Lambda.Core;
