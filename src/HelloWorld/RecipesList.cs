e898d216a8b11a74b8d39341140288acd6fbe97d
MarkSelyukov <33294280+MarkSelyukov@users.noreply.github.com>
2020-02-12 00:28:47 -0600

    Cleaned up the code a lot and it still works. Mind = blown

--- src/HelloWorld/RecipesList.cs
+++ src/HelloWorld/RecipesList.cs
@@ -17,27 +17,14 @@ namespace HelloWorld
             UrlMaker urlMaker = new UrlMaker(recipeSearch);
             this.recipeListUrl = urlMaker.UrlCreation;
             this.returnedRecipesString = new WebClient().DownloadString(this.recipeListUrl);
-            ParseJson(recipeResults, urlMaker);
+            ParseJson(recipeResults);
         }
 
-        public void ParseJson(Recipe recipeResults, UrlMaker urlMaker)
+        public void ParseJson(Recipe recipeResults)
         {
             recipeResults = JsonConvert.DeserializeObject<Recipe>(this.returnedRecipesString);
 
-            for (int i = 0; i < Int32.Parse(urlMaker.searchAmount); i++)
-            {
-                //I have to do it like this because I was getting errors
-                string id = recipeResults.results[i].GetValue("id").ToString();
-                string title = recipeResults.results[i].GetValue("title").ToString();
-                string readyInMinutes = recipeResults.results[i].GetValue("readyInMinutes").ToString();
-                string serving = recipeResults.results[i].GetValue("servings").ToString();
-                string image = recipeResults.results[i].GetValue("image").ToString();
-                string[] imageUrls = {"WOOP"}; //recipeResults.results[i].GetValue("imageUrls").ToObject<string[]>();
-
-                Recipe recipe = new Recipe(id, title, readyInMinutes, serving, image, imageUrls);
-
-                recipes.Add(recipe);
-            }
+            recipes.Add(recipeResults);
         }
     }
 }
